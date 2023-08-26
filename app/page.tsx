2f95f7a901239a13aa38d4af4d3d45115e52c0b6
krishna <krishna.sannidhi@gmail.com>
2023-08-26 18:03:19 -0500

    added expermiental flag

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,47 @@
-import Image from "next/image";
+type Product = {
+  id?: number;
+  product: string;
+  price: string;
+};
+export default async function Home() {
+  const results = await fetch(
+    "https://64ea81b0bf99bdcc8e6796ef.mockapi.io/products",
+    {
+      cache: "no-cache",
+    }
+  );
 
-export default function Home() {
+  const products: Product[] = await results.json();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {" "}
-      Server Actions Tutotrial
+    <main className="">
+      <h1 className="text-3xl font-bold text-center">Products Warehouse</h1>
+
+      <form action="" className="flex flex-col gap-5 max-w-xl mx-auto p-5">
+        <input
+          type="text"
+          className="border border-gray-300 p-2 rounded-md"
+          placeholder="Enter Product name..."
+        />
+        <input
+          type="text"
+          className="border border-gray-300 p-2 rounded-md"
+          placeholder="Enter Price name..."
+        />
+        <button className="border bg-blue-500 text-white p-2 rounded-md">
+          Add Product
+        </button>
+      </form>
+
+      <h2 className="font-bold p-5">List of Products</h2>
+
+      <div className="flex flex-wrap gap-5">
+        {products.map((product) => (
+          <div className="p-5 shadow" key={product?.id}>
+            <p>{product?.product}</p>
+            <p>£{product?.price}</p>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
